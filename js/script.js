fb29ed5ba827c5eb3fd3c29bd7326284a96e496d
MarceloHenrique7 <marcelohenriquex77@gmail.com>
2024-02-17 18:44:22 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+const buttonSubmit = document.querySelector(".button__submit");
+
+
+function handleButtonSubmit () {
+
+    setTimeout(() => {
+        buttonSubmit.addEventListener("click", () => {
+            window.alert("Formulario Enviado - verifique seu e-mail, para receber promoções")
+        })
+    }, 3000)
+    
+}
+
+
+handleButtonSubmit();
+
+
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+    
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav__list",
+    ".nav__list li",
+  );
+  mobileNavbar.init();
